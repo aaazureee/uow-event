71e6720f9e194fbd9e32b6ddf6f30558c44d161e
Long Hung Nguyen <longhungn98@gmail.com>
2018-04-08 21:03:00 +1000

    Finish backend for homepage

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
 
 import homeRouter from './routes/home';
 import eventRouter from './routes/event';
@@ -7,10 +8,12 @@ import eventRouter from './routes/event';
 const app = express();
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 const dbuser = 'test';
 const dbpassword = 'test';
-mongoose.connect(`mongodb://${dbuser}:${dbpassword}@ds233769.mlab.com:33769/dev-event`);
+mongoose.connect(`mongodb://${dbuser}:${dbpassword}@ds115569.mlab.com:15569/event`);
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 db.on('open', () => console.log('Connection to database established'));
--- routes/home/index.js
+++ routes/home/index.js
@@ -6,22 +6,19 @@ import Event from '../../models/event';
 
 router.get('/', (req, res, next) => {
     Event.find().then((result)=>{
-        for (let event of result) {
+        let events = result.map((value)=>{
+            let event = value.toObject();
             if (!event.price) {
                 event.price = "Free";
             }
             let fromDate = moment(event.startDate).format('ddd D MMM YYYY, hh:mmA');
             let toDate = moment(event.endDate).format('hh:mmA');
             event.durationString = `${fromDate} - ${toDate}`;
+            return event;
+        });
 
-        }
-        res.render('index', {events: result});
+        res.render('index', {events: events});
     });
 });
-
-router.get('/test', (req, res, next) => {
-    res.send('Testing');
-});
-
 
 export default router;
