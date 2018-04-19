dc5875e22878099630dc5fb1be61e1755971e28c
Long Hung Nguyen <longhungn98@gmail.com>
2018-04-19 22:42:35 +1000

    add backend for booked events

--- routes/home/booked.js
+++ routes/home/booked.js
@@ -1,10 +1,34 @@
 import express from 'express';
+import User from '../../models/user';
+import Event from '../../models/event';
+import { parseEvents } from '../common/eventParser';
 
 const router = express.Router();
 
-router.get('/booked-events', (req, res) => {
-  res.locals.options.page = 'booked-events';
-  res.render('booked', res.locals.options);
+router.get('/booked-events', async (req, res, next) => {
+  try {
+    let user = await User.findOne({
+      username: res.locals.options.username
+    });
+
+    let events = await Event.find({
+      eventId: {
+        $in: user.eventsBooked
+      },
+      endDate: {
+        $gt: new Date()
+      }
+    }).sort({
+      startDate: 1
+    });
+  
+    res.locals.options.events = parseEvents(events);
+  
+    res.locals.options.page = 'booked-events';
+    res.render('booked', res.locals.options);
+  } catch (err) {
+    next(err);
+  }
 });
 
 export default router;
